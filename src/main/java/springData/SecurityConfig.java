4593ce033f6c8a157fd5667bb7eb4885da3765f6
Mohamed <dtmohamed95@gmail.com>
2018-12-04 09:34:34 +0000

    Part1:- (Secure Connection using Https) complete

---
+++ src/main/java/springData/SecurityConfig.java
@@ -0,0 +1,13 @@
+package springData;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // Part1: Secure Connection
+        http.requiresChannel().anyRequest().requiresSecure();
+    }
+}

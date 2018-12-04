e3b7226afcf5663357f34db2ce43a2de6bf187b3
Mohamed <dtmohamed95@gmail.com>
2018-12-04 09:50:39 +0000

    part2: 3-4

--- src/main/java/springData/SecurityConfig.java
+++ src/main/java/springData/SecurityConfig.java
@@ -2,12 +2,35 @@ package springData;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // Part1: Secure Connection
-        http.requiresChannel().anyRequest().requiresSecure();
+        http.requiresChannel().anyRequest().requiresSecure()
+            .and()
+                    .formLogin()
+                                .loginPage("/user-login")
+                                .loginProcessingUrl("/login")
+                                .defaultSuccessUrl("/success-login",true)
+                                .failureUrl("/error-login")
+                                .permitAll()
+            .and()
+                    .logout()
+                                .invalidateHttpSession(true)
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                                .logoutSuccessUrl("/user-login")
+                                .permitAll()
+            .and()
+                    .authorizeRequests()
+                                .antMatchers("/admin/**").hasRole("ADMIN")
+                                .antMatchers("/create/**").hasRole("MANAGER")
+                                .antMatchers("/delete/**").hasRole("MANAGER")
+                                .antMatchers("/list/**").hasAnyRole("ASSISTANT","MANAGER")
+            .and()
+                    .exceptionHandling().accessDeniedPage("/access-denied");
+
     }
 }
